b026c806ece73015d6a8d5a1d4ecbede3fa590c6
Alexander <kkruscherr@gmail.com>
2024-07-13 14:03:50 +0300

    Up to Sprint 13; Lesson 10; Theory Over

--- cats/serializers.py
+++ cats/serializers.py
@@ -2,19 +2,21 @@ import datetime
 
 from rest_framework import serializers
 
-from .models import Achievement, AchievementCat, Cat, Owner
+from .models import CHOICES, Achievement, AchievementCat, Cat, Owner
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
