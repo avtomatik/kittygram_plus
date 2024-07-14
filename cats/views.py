7d202bd062f95d1170e03fef710e863aeff10824
Alexander <kkruscherr@gmail.com>
2024-07-14 13:47:46 +0300

    Up to Sprint 13; Lesson 12

--- cats/views.py
+++ cats/views.py
@@ -1,44 +1,24 @@
-from rest_framework import mixins, viewsets
-from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from .models import Cat, Owner
-from .serializers import CatListSerializer, CatSerializer, OwnerSerializer
+from .serializers import CatSerializer, OwnerSerializer
 
 
-class CatViewSet(viewsets.ModelViewSet):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
-
-    @action(detail=False, url_path='recent-white-cats')
-    def recent_white_cats(self, request):
-        cats = Cat.objects.filter(colr='White')[:5]
-        serializer = self.get_serializer(cats, many=True)
+class CatViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Cat.objects.all()
+        serializer = CatSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return CatListSerializer
-        return CatSerializer
+    def retrieve(self, request, pk=None):
+        queryset = Cat.objects.all()
+        cat = get_object_or_404(queryset, pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
-
-
-class UpdateDeleteViewSet(
-    mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
-):
-    pass
-
-
-class CreateRetrieveViewSet(
-    mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
-):
-    pass
-
-
-class LightCatViewSet(CreateRetrieveViewSet):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
