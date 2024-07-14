7fd9711afd793c82dffda2dc6ce9f60549f83903
Alexander <kkruscherr@gmail.com>
2024-07-14 13:45:42 +0300

    Up to Sprint 13; Lesson 12; Not Complete
    All But `Мой вьюсет — мои правила: наследование от класса ViewSet`

--- cats/views.py
+++ cats/views.py
@@ -1,14 +1,44 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .serializers import CatListSerializer, CatSerializer, OwnerSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(colr='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CatListSerializer
+        return CatSerializer
+
 
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
+
+
+class UpdateDeleteViewSet(
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
+):
+    pass
+
+
+class CreateRetrieveViewSet(
+    mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
