1604568ddcb644ef1a2f91b03f2722d23c42bb34
Alexander <kkruscherr@gmail.com>
2024-07-14 14:28:40 +0300

    Up to Sprint 13; Lesson 13; Theory Complete

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from cats.views import CatViewSet, OwnerViewSet
@@ -10,6 +9,6 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
-
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
