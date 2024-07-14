7824a107c405e3faa9dac042660cadc91cddb9c9
Alexander <kkruscherr@gmail.com>
2024-07-14 14:08:49 +0300

    DRF Authtoken Exercise

--- cats/views.py
+++ cats/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .serializers import CatListSerializer, CatSerializer, OwnerSerializer
 
 
-class CatViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = Cat.objects.all()
-        serializer = CatSerializer(queryset, many=True)
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(colr='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = Cat.objects.all()
-        cat = get_object_or_404(queryset, pk=pk)
-        serializer = CatSerializer(cat)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CatListSerializer
+        return CatSerializer
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
